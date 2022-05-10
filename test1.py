3f194cc70be267bd6a67cd3b1078d538ea32c20e
瑞文h <2578592505@qq.com>
2022-05-10 13:51:28 +0800

    message:"AssertionError: '1: Buy peacock feathers' not found in ['1:Use peacock feathers to make a fly']"

--- test1.py
+++ test1.py
@@ -33,7 +33,8 @@ class NewVisitorTest(unittest.TestCase):
                 )
                 # She types "Buy peacock feathers" into a text box (Edith's hobby
                 # is tying fly-fishing lures)
-                inputbox.send_keys('Buy peacock feathers')
+                #inputbox.send_keys('Buy peacock feathers')
+                inputbox.send_keys('Use peacock feathers to make a fly')
                 # When she hits enter, the page updates, and now the page lists # "1: Buy peacock feathers" as an item in a to-do list
                 inputbox.send_keys(Keys.ENTER)
                 time.sleep(1)
@@ -41,11 +42,16 @@ class NewVisitorTest(unittest.TestCase):
                 #table = self.browser.find_element_by_id('id_list_table')
                 table = self.browser.find_element(By.ID, 'id_list_table')
                 rows = table.find_elements_by_tag_name('tr')
+                """
                 self.assertTrue(
                         any(row.text == '1: Buy peacock feathers' for row in rows),
-                        "New to-do item did not appear in table"
+                        f"New to-do item did not appear in table.Contexts were:\n{table.text}"
                 )
-
+                """
+                self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+                self.assertIn(
+                        '2: Use peacock feathers to make a fly', 
+                        [row.text for row in rows])
                 # There is still a text box inviting her to add another item. She# enters "Use peacock feathers to make a fly" (Edith is very methodical)
                 # enters "Use peacock feathers to make a fly"(Edith is very methodical)
                 self.fail('Finish the test!')
