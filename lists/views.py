49c52254b04b78353c2ec98762e5e4ed4da17ba3
瑞文h <2578592505@qq.com>
2022-05-31 14:09:04 +0800

    "chapter 7 51 page"

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item,List
 # Create your views here.
 #def home_page(request):
  #   return  HttpResponse('<html><title>To-Do lists</title></html>')
@@ -17,7 +17,8 @@ def home_page(request):
     return render(request, 'home.html')
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_=List.objects.create()
+    Item.objects.create(text=request.POST['item_text'],list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 """
 def home_page(request):
